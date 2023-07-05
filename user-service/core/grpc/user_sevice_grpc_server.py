ba7c5fb41e4a51248a1d3f53006f37bcada7e87d
Arseny <20mineslend14@gmail.com>
2023-07-05 12:55:57 +0500

    add grpc to user-service

---
+++ user-service/core/grpc/user_sevice_grpc_server.py
@@ -0,0 +1,33 @@
+from concurrent import futures
+
+import grpс
+
+import user_service_pb2
+import user_service_pb2_grpc
+from ..deps import ComponentsContainer
+
+ioc = ComponentsContainer()
+user_service = ioc.user_service
+
+
+class UserServiceServer(user_service_pb2_grpc.UserServiceServicer):
+
+    def getUser(self, request, context):
+        id = request.id
+        user = user_service.get_user_by_id(id)
+        response = user_service_pb2.UserRequest()
+        response.id = user.id
+        response.chat_id = user.chat_id
+        response.username = user.username
+        return response
+
+    def getUsersByGroup(self, request, context):
+        return super().getUsersByGroup(request, context)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    user_service_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
