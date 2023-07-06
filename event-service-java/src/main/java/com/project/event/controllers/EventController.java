a968fd1438367ea1f9116e6c12556e0efa455975
Arseny <20mineslend14@gmail.com>
2023-07-06 22:05:55 +0500

    add swagger docs to event service

--- event-service-java/src/main/java/com/project/event/controllers/EventController.java
+++ event-service-java/src/main/java/com/project/event/controllers/EventController.java
@@ -2,6 +2,13 @@ package com.project.event.controllers;
 
 import com.project.event.models.Event;
 import com.project.event.services.EventService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/event")
+@Tag(name = "Event service")
 public class EventController {
     private final EventService service;
 
@@ -17,22 +25,79 @@ public class EventController {
         this.service = service;
     }
 
+    @Operation(summary = "Получение всех событий")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(
+                                            schema = @Schema(
+                                                    implementation = Event.class
+                                            )
+                                    )
+                            )
+                    }
+            )
+    })
     @GetMapping
     public ResponseEntity<?> getAllEvents() {
         return ResponseEntity.ok(service.getAllEvents());
     }
 
+
+    @Operation(summary = "Получение события по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = Event.class
+                                    )
+                            )
+                    }
+            )
+    })
     @GetMapping("/{eventId}")
     public ResponseEntity<?> getEventById(@PathVariable("eventId") long eventId) {
         return ResponseEntity.ok(service.getEventById(eventId));
     }
 
+    @Operation(summary = "Создание события",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = Event.class
+                            )
+                    )
+            ))
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201"
+            )
+    })
     @PostMapping
     public ResponseEntity<?> createEvent(@RequestBody Event event) {
         service.createEvent(event);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Обновление события по id",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = Event.class
+                            )
+                    )
+            ))
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200"
+            )
+    })
     @PutMapping("/{eventId}")
     public ResponseEntity<?> updateEvent(@PathVariable("eventId") long eventId,
                                          @RequestBody Event event) {
@@ -41,6 +106,12 @@ public class EventController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Удаление события")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200"
+            )
+    })
     @DeleteMapping("/{eventId}")
     public ResponseEntity<?> deleteEvent(@PathVariable("eventId") long eventId) {
         service.deleteEventById(eventId);
--- event-service/core/exceptions/illegal_argument_exception.py
+++
@@ -1,4 +0,0 @@
-class IllegalArgumentException(RuntimeError):
-    def __init__(self, message: str, *args: object) -> None:
-        self.message = message
-        super().__init__(*args)
