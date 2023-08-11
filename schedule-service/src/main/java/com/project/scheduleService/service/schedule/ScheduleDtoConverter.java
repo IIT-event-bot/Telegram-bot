2b90172a443f33021fc575afe38c274062d7f17d
Arseny <20mineslend14@gmail.com>
2023-08-12 00:20:40 +0500

    fix sorting schedule dto

--- schedule-service/src/main/java/com/project/scheduleService/service/schedule/ScheduleDtoConverter.java
+++ schedule-service/src/main/java/com/project/scheduleService/service/schedule/ScheduleDtoConverter.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -61,6 +62,7 @@ public class ScheduleDtoConverter implements ScheduleDtoMapper {
                     convertLessons(dayLessons)
             ));
         }
+        days.sort(Comparator.comparing(a -> DayType.valueOf(a.title())));
         return days;
     }
 
@@ -109,6 +111,7 @@ public class ScheduleDtoConverter implements ScheduleDtoMapper {
     public List<LessonDto> convertLessons(List<Lesson> lessons) {
         return lessons.stream()
                 .map(this::convertLesson)
+                .sorted(Comparator.comparing(LessonDto::timeStart))
                 .toList();
     }
 
